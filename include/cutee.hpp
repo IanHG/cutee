40fc7d6e99b225175df13affaeb3b2a86024ad2a
Ian Heide Godtliebsen <ian@chem.au.dk>
2019-03-04 12:09:09 +0100

    updated cmake
    Will now install headers in <include>/cutee except for main header
    including everything, which is still located in <include>
    
    fixes #3

--- include/cutee.hpp
+++ include/cutee.hpp
@@ -1,15 +1,15 @@
 #ifndef CUTEE_HPP_INCLUDED
 #define CUTEE_HPP_INCLUDED
 
-#include "failed_data.hpp"
-#include "float_eq.hpp"
-#include "performance_test.hpp"
-#include "collection.hpp"
-#include "test_failed.hpp"
-#include "test_suite.hpp"
-#include "timer.hpp"
-#include "unit_assert.hpp"
-#include "unit_test_factory.hpp"
-#include "unit_test.hpp"
+#include "cutee/failed_data.hpp"
+#include "cutee/float_eq.hpp"
+#include "cutee/performance_test.hpp"
+#include "cutee/collection.hpp"
+#include "cutee/test_failed.hpp"
+#include "cutee/test_suite.hpp"
+#include "cutee/timer.hpp"
+#include "cutee/unit_assert.hpp"
+#include "cutee/unit_test_factory.hpp"
+#include "cutee/unit_test.hpp"
 
 #endif /* CUTEE_HPP_INCLUDED */
